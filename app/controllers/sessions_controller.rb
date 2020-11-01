d8d5e0957fdfd070b0c0ef28d221b385cfafb84b
Hyakon <hyakon@pop-os.localdomain>
2020-11-01 20:25:33 +0100

    remove console line

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
   def new
-    console
   end
 
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   end
 
   def new
-    console
     @user = User.new
   end
 
